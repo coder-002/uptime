7f7d8e6cd664298b67f7ee1eff8236badef9f36a
coder-002 <markoshprajapati@gmail.com>
2026-01-28 15:07:52 +0545

    adding axum

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 
 use std::time::Duration;
 mod monitor;
-use axum::Router;
-use axum::routing::get;
 use monitor::monitor::*;
 use monitor::monitor_engine::*;
 
@@ -11,14 +9,6 @@ use monitor::monitor_engine::*;
 #[tokio::main]
 
 async fn main(){
-
-        let app = Router::new().route("/", get(|| async{"Hello, world"}) );
-
-        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-        axum::serve(listener, app).await.unwrap();
-
-
 
     let monitors = vec![
         Monitor{id: 1, name: "Google".into(), url: "https://google.com".into(), interval_secs:5, timeout_secs:5, monitor_type: MonitorType::Http { method: HttpMethod::Get, keyword: None }, retries: 1},
@@ -31,12 +21,9 @@ async fn main(){
     for m in monitors{
         tokio::spawn(run_monitor(m));
     }
-
- 
+    
     loop{
         tokio::time::sleep(Duration::from_secs(5)).await;
     }
-
-
    
 }
